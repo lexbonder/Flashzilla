2a8c5091beb41fc69f25a372c5620733f85cae2e
Alex Bonder <lexbonder@gmail.com>
2023-09-25 08:32:01 -0400

    Initial Commit

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Alex Bonder on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Alex Bonder on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
