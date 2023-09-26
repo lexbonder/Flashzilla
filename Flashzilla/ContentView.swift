b93989abb2968973d6509af6fa24c4931e8fd7b0
Alex Bonder <lexbonder@gmail.com>
2023-09-26 07:08:01 -0400

    Hacking with Swift - 100 Days of SwiftUI - HitTesting

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 300, height: 300)
+                .onTapGesture {
+                    print("square tapped")
+                }
+            Circle()
+                .fill(.red)
+                .frame(width: 300, height: 300)
+                .contentShape(Rectangle()) // changes tappable area to whole frame
+                .onTapGesture {
+                    print("circle tapped")
+                }
+//                .allowsHitTesting(false) // ignores tap gestures, passes along to whatever is behind it.
+            
         }
         .padding()
     }
