552c15145abe5bfd0f8baac0a885b686252c178a
Alex Bonder <lexbonder@gmail.com>
2023-09-27 18:34:44 -0400

    Hacking with Swift - 100 Days of SwiftUI - Checking and responding to A11y settings

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
+//    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor // returns boolean, assign different colors to things.
+
+//    @Environment(\.accessibilityReduceMotion) var reduceMotion // returns boolean
+//    @State private var scale = 1.0
+//    withOptionalAnimation {
+//        scale *= 1.5
+//    }
+
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency // returns boolean
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello World")
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
     }
 }
 
