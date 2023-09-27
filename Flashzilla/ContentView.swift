6f653e249b7bf8adfe2bc108b69834d07eda2c8c
Alex Bonder <lexbonder@gmail.com>
2023-09-27 17:33:17 -0400

    Hacking with Swift - 100 Days of SwiftUI - Timer
    Repeating timer, tolerance, cancelling

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, world!")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                    print("Cancelled!!")
+                } else {
+                    print("The time is now \(time)")
+                }
+                counter += 1
+            }
     }
 }
 
